efe2a791cc7cca212957ce0902421708165e2f32
Daniel Brännvall <daniel@brannvall.net>
2020-12-04 13:33:45 +0100

    "Benchmarking" is fun

--- src/AoC.hs
+++ src/AoC.hs
@@ -2,9 +2,17 @@
 
 module AoC
     ( apply
+    , applyN
     )
 where
+
+import           Control.Monad                  ( forM_ )
 
 apply f fn = do
     input <- lines <$> readFile fn
     putStrLn $ show $ f $ input
+
+applyN :: ([String] -> Int) -> String -> Int -> IO ()
+applyN f fn n = do
+    input <- lines <$> readFile fn
+    forM_ [1 .. n] (\_ -> seq (f input) (return ()))
